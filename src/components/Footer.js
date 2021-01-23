02ee31ad62cbd761077cb160edb746b02dc83a34
Luis Canahuate <luis.canahuate16@gmail.com>
2021-01-22 22:12:09 -0500

    Footer component with basic styling and href links

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className="m-10 text-sm text-center text-black-500">
+            <p>Employee pictures provided by <a href="https://randomuser.me/" className="font-bold text-black-600 hover:text-green-500">Randomuser.Me</a></p>
+            <p>Built by <a href="https://canahuate16.github.io/" className="font-bold text-black-600 hover:text-green-500">Luis Canahuate</a></p>
+        </div>
+    )
+}
+
+export default Footer;
